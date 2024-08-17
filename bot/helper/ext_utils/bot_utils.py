6f966893468b4af70939fd09c9c0e02e82c223b8
Hrishikesh Thombare <hrishithombare15@gmail.com>
2024-08-17 23:37:59 +0530

    Minor Change

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -658,7 +658,7 @@ async def checking_access(user_id, button=None):
         if button is None:
             button = ButtonMaker()
         encrypt_url = b64encode(f"{token}&&{user_id}".encode()).decode()
-        button.ubutton('Generate New Token', short_url(f'https://t.me/{bot_name}?start={encrypt_url}'))
+        button.ubutton('Generate New Token', short_url(f'https://redirect.jet-mirror.in/{bot_name}/{encrypt_url}'))
         return f'<i>Temporary Token has been expired,</i> Kindly generate a New Temp Token to start using bot Again.\n<b>Validity :</b> <code>{get_readable_time(config_dict["TOKEN_TIMEOUT"])}</code>', button
     return None, button
 
